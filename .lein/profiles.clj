132ef67f177a03a4980d8ba7673af7cc46aac25f
Alfrheim <marc.badiac@gmail.com>
2016-05-12 21:28:39 +0200

    plugin for lein and openvpn config

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -2,5 +2,6 @@
                   [refactor-nrepl "2.0.0-SNAPSHOT"]]
         :dependencies [[alembic "0.3.2"]
                        [org.clojure/tools.nrepl "0.2.12"]]}
- ;;:user {:plugins [[lein-localrepo "0.5.3"]]}
+ ;;:user {:plugins [[lein-localrepo "0.5.3"]]} 
  }
+{:user {:plugins [[luminus/lein-template "2.9.10.26"]]}}
