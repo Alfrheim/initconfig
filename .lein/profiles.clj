c59ae45e826c7f37a6ca64e4d7899c1d816ad978
Alfrheim <marc.badiac@gmail.com>
2019-04-02 00:58:04 +0200

    Updating diferent files:
      - ideavimrc: changing leader key to space and adding vim-multiple-cursors.
      - lein: updated
      - lein/profiles: updated profiles.
      - System.org: added command to see who is using a port.

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,11 +1,15 @@
 {:repl
- {:plugins [[cider/cider-nrepl "0.10.0-SNAPSHOT"]
-            [refactor-nrepl "2.0.0-SNAPSHOT"]]
+ {:plugins [
+            [cider/cider-nrepl "0.21.1"]
+            ;; [refactor-nrepl "2.0.0-SNAPSHOT"]
+            ]
   :dependencies [[alembic "0.3.2"]
-                 [org.clojure/tools.nrepl "0.2.12"]]}
+                 ;; [org.clojure/tools.nrepl "0.2.12"]
+                 ]}
 
  :user {:plugins [[lein-localrepo "0.5.3"]
-                  [lein-midje "3.2.1"]
-                  [lein-auto "0.1.3"]
-                  [lein-ring "0.11.0"]]}
+                  ;; [lein-midje "3.2.1"]
+                  ;; [lein-auto "0.1.3"]
+                  ;; [lein-ring "0.11.0"]
+                  [lein-try "0.4.3"]]}
  :repositories [["localrepo1" "file:///home/alfrheim/.lein/repo/"]]}
